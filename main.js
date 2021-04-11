a0e17e68724a35216a01f339caeec0ca2ae9f040
skoech <sharonkoech5147@gmail.com>
2021-04-11 16:11:28 +0300

    Making a web pag responsive with CSS media queries, and a navbar functional using VanillaJS.

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navBarLinks = document.getElementsByClassName('nav-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button');
+const navBarLinks = document.getElementsByClassName('nav-links');
+
+function toggleButton () {
+    toggleButton.classList.toggle('nav-active');
+}
 
 toggleButton.addEventListener('click', ()=> {
     navBarLinks.classList.toggle('active')
+    toggleButton();
 })
+
+
+
+// Bonus: Creating a Smart Navbar
+
+let previousScrolledPosition = 0;
+
+const isScrollingDown = function () {
+    let currentScrolledPosition = window.pageYOffset || window.scrollY;
+    let scrollingDown;
+
+    if (currentScrolledPosition > previousScrolledPosition) {
+        scrollingDown = true;
+    } else {
+        scrollingDown = false;
+    }
+    currentScrolledPosition = previousScrolledPosition;
+    return scrollingDown;
+}
+
+const nav = document.querySelector('nav');
+
+const handleNavScroll = function () {
+    if (isScrollingDown()) {
+        nav.classList.add('scroll-down');
+        nav.classList.remove('scroll-up');
+    } else {
+        nav.classList.add('scroll-up');
+        nav.classList.remove('scroll-down')
+    }
+}
